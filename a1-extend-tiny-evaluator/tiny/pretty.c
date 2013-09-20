2a08bc514ab09d0c41dfcc1d2385198a3c6978c0
ftufek <furkan._t@hotmail.com>
2013-09-20 15:28:48 -0400

    Pretty printing only the necessary parentheses

--- a1-extend-tiny-evaluator/tiny/pretty.c
+++ a1-extend-tiny-evaluator/tiny/pretty.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include "pretty.h"
- 
-void prettyEXP(EXP *e)
+
+typedef enum
+{ min,
+  abs,
+  plus_minus, /* +, - */
+  mult_div_mod, /* *, /, % */
+  power /* ** */
+} priority;
+
+typedef enum
+{ false,
+  true
+} bool;
+
+#define HIPRI(cpri, parent) if (pri > cpri){printf(parent);} \
+		else if(pri == cpri && leftChild == false){printf(parent);}
+#define OPEN_PAREN(cpri) HIPRI(cpri, "(");
+#define CLOSE_PAREN(cpri) HIPRI(cpri, ")");
+
+static void _prettyEXP(EXP *e, priority pri, bool leftChild)
 { switch (e->kind) {
     case idK:
          printf("%s",e->val.idE);
@@ -10,50 +28,59 @@ void prettyEXP(EXP *e)
          printf("%i",e->val.intconstE);
          break;
     case timesK:
-         printf("(");
-         prettyEXP(e->val.timesE.left);
+         OPEN_PAREN(mult_div_mod)
+         _prettyEXP(e->val.timesE.left, mult_div_mod, true);
          printf("*");
-         prettyEXP(e->val.timesE.right);
-         printf(")");
+         _prettyEXP(e->val.timesE.right, mult_div_mod, false);
+         CLOSE_PAREN(mult_div_mod)
          break;
     case divK:
-         printf("(");
-         prettyEXP(e->val.divE.left);
+    	 OPEN_PAREN(mult_div_mod)
+         _prettyEXP(e->val.divE.left, mult_div_mod, true);
          printf("/");
-         prettyEXP(e->val.divE.right);
-         printf(")");
+         _prettyEXP(e->val.divE.right, mult_div_mod, false);
+         CLOSE_PAREN(mult_div_mod)
          break;
     case moduloK:
-    	 printf("(");
-    	 prettyEXP(e->val.moduloE.left);
+    	 OPEN_PAREN(mult_div_mod)
+    	 _prettyEXP(e->val.moduloE.left, mult_div_mod, true);
     	 printf("%%");
-    	 prettyEXP(e->val.moduloE.right);
-    	 printf(")");
+    	 _prettyEXP(e->val.moduloE.right, mult_div_mod, false);
+    	 CLOSE_PAREN(mult_div_mod)
     	 break;
     case plusK:
-         printf("(");
-         prettyEXP(e->val.plusE.left);
+    	 OPEN_PAREN(plus_minus)
+         _prettyEXP(e->val.plusE.left, plus_minus, true);
          printf("+");
-         prettyEXP(e->val.plusE.right);
-         printf(")");
+         _prettyEXP(e->val.plusE.right, plus_minus, false);
+         CLOSE_PAREN(plus_minus)
          break;
     case minusK:
-         printf("(");
-         prettyEXP(e->val.minusE.left);
+         OPEN_PAREN(plus_minus)
+         _prettyEXP(e->val.minusE.left, plus_minus, true);
          printf("-");
-         prettyEXP(e->val.minusE.right);
-         printf(")");
+         _prettyEXP(e->val.minusE.right, plus_minus, false);
+         CLOSE_PAREN(plus_minus)
          break;
     case powerK:
-    	 printf("(");
-		 prettyEXP(e->val.powerE.left);
+    	 OPEN_PAREN(power)
+		 _prettyEXP(e->val.powerE.left, power, true);
 		 printf("**");
-		 prettyEXP(e->val.powerE.right);
-		 printf(")");
+		 _prettyEXP(e->val.powerE.right, power, false);
+		 CLOSE_PAREN(power)
     	 break;
     case absoluteK:
-    	 printf("abs");
-    	 prettyEXP(e->val.absoluteE.inside);
+    	 printf("abs(");
+    	 _prettyEXP(e->val.absoluteE.inside, abs, true);
+    	 printf(")");
     	 break;
   }
+}
+
+#undef HIPRI
+#undef OPEN_PAREN
+#undef CLOSE_PAREN
+
+void prettyEXP(EXP *e){
+	_prettyEXP(e, min, true);
 }
