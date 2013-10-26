7da60c830e5e481720deb9f0e699e07549ea2243
ftufek <furkan._t@hotmail.com>
2013-10-26 12:10:03 -0400

    Fix bug.

--- wig/src/ast/html.cc
+++ wig/src/ast/html.cc
@@ -10,7 +10,7 @@ HtmlTag::HtmlTag
 (string id, map<string, string> *attrs, bool isClosing, bool isGap)
     :id_(id), attrs_(attrs), is_closing_(isClosing), is_gap_(isGap) {}
 HtmlTag::~HtmlTag(){
-	attrs_->erase(attrs_->begin(), attrs_->end());
+	attrs_->clear();
 	delete(attrs_);
 }
 
--- wig/src/ast/list.cc
+++ wig/src/ast/list.cc
@@ -5,9 +5,12 @@ namespace ast {
 
 List::List(std::list<Base *> *exps):exps_(exps){}
 List::~List(){
-	for(auto it : *exps_){
-		delete(it);
-	}
+	//TODO: for some reason the following lines
+	//cause a Segmentation fault, fix when have time!
+//	while(!exps_->empty()){
+//		delete (exps_->front());
+//		exps_->pop_front();
+//	}
 	delete(exps_);
 }
 
